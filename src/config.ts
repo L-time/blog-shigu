fde273b8ba9a0591dd8a6516ffa08b3a02c12892
shigu666 <shigu_@outlook.com>
2025-08-29 23:36:27 +0800

    修改打错的画师id

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
-			text: "okamiriru", // Credit text to be displayed
+			text: "ookamiriru", // Credit text to be displayed
 			url: "https://x.com/ookamiriru", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
